0fc35991f066096939888b53fd072f852da49f60
cybertekschool <github@cybertekschool.com>
2020-10-10 13:43:55 -0400

    Java Review

--- src/main/java/com/cybertek/Main.java
+++ src/main/java/com/cybertek/Main.java
@@ -1,4 +1,21 @@
 package com.cybertek;
 
+import com.cybertek.core.Methods;
+
 public class Main {
+    public static void main(String[] args) {
+
+        //*******CORE*******//
+        Methods methods = new Methods();
+        methods.methodA();
+        methods.methodA("Apple");
+        System.out.println(methods.methodB());
+        System.out.println(methods.methodB(6));
+        Methods.methodC();
+
+
+
+
+    }
+
 }
