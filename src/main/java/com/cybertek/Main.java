dd1b18f70fd976ea8350a70e016a567d05d2d0ba
cybertekschool <github@cybertekschool.com>
2020-10-13 18:51:58 -0400

    Java Review

--- src/main/java/com/cybertek/Main.java
+++ src/main/java/com/cybertek/Main.java
@@ -5,6 +5,10 @@ import com.cybertek.core.Methods;
 import com.cybertek.core.SelectionStatements;
 import com.cybertek.oop.encapsulation.Role;
 import com.cybertek.oop.encapsulation.User;
+import com.cybertek.oop.inheritance.Project;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 public class Main {
@@ -31,8 +35,33 @@ public class Main {
         System.out.println(user.getRole().getDescription());
 
         //*******OOP-Inheritance*******//
+        Project project = new Project(1,LocalDateTime.now(),1,LocalDateTime.now().minusHours(5),1,"PRJ001", "Human Resource CRM",
+                new User("Mike","Smith",new Role(1,"Manager")),
+                LocalDate.now(),LocalDate.now().plusMonths(6),"IN_PROGRESS","HCRM Detail Information");
 
-        //Project Object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
