c1489a6a61bda8bc77bbe4b1c46ceacfdb881c08
cybertekschool <github@cybertekschool.com>
2020-10-13 20:22:54 -0400

    Java Review

--- src/main/java/com/cybertek/enums/Status.java
+++ src/main/java/com/cybertek/enums/Status.java
@@ -1,15 +1,13 @@
 package com.cybertek.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum Status {
 
     OPEN("Open"),IN_PROGRESS("In Progress"),UAT_TEST("UAT Testing"),COMPLETED("Completed");
-
     private final String value;
 
-    Status(String value){
-        this.value=value;
-    }
 }
